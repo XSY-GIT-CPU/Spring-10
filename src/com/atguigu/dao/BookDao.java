dde0d1559e575b8e00cb133c0a16998422b39f09
xudada <xudada66@alyun.com>
2020-09-27 19:52:25 +0800

    整合idea的第一次提交

---
+++ src/com/atguigu/dao/BookDao.java
@@ -0,0 +1,16 @@
+package com.atguigu.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookDao {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public void updateBook(){
+        jdbcTemplate.update("update book set `name`='图书表被修改'");
+    }
+}
